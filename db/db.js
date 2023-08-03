208762bb826c220399fcd377f5756f09f41dc468
Amina-Moufakkir <aminamouf22@gmail.com>
2023-08-03 08:42:24 -0400

    refactor db

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({ path: 'MONGO_URI' });
 const db = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
